f157ce2be35935cddf66db9a6fc100e3f9be3aea
Anurag Chauhan <nanuchauhan2006@gmail.com>
2025-10-22 14:41:59 +0530

    Add files via upload

---
+++ namaste.c
@@ -0,0 +1,19 @@
+//how its usefull
+/*functions are used to do repitative tasks 
+
+*/
+#include <stdio.h>
+void namaste(){
+    printf(" namaste \n");
+    printf("kaise ho?\n");
+    return ;
+
+}
+
+int main(){
+    namaste();
+    namaste();
+    namaste();
+
+return 0;
+}
